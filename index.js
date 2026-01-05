0d0a539e7bdd1eaef4352e2531f1448759b9e774
sarvesh hastak <sarveshhastak1309@gmail.com>
2026-01-05 15:08:23 +0530

    frontend url added

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ require("dotenv").config()
 
 const app = express()
 app.use(express.json())
-app.use(cors({ origin: "http://localhost:5173", credentials: true }))
+app.use(cors({ origin: "https://chatbot-frontend-gamma-opal.vercel.app", credentials: true }))
 app.use(cookieParser())
 app.use("/api/auth", require("./routes/auth.route"))
 app.use("/api/chat", userProtected, require("./routes/chat.route"))
